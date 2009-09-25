d35b6ece9d0979f30f9ccfecbe02d2f87c9e06b7
Peter Palfrader <peter@palfrader.org>
2009-09-25 15:22:22 +0200

    convert (empty) tests to 0.9.7 style

--- web/app/snapshot/tests/functional/test_archive.py
+++ web/app/snapshot/tests/functional/test_archive.py
@@ -3,5 +3,5 @@ from snapshot.tests import *
 class TestArchiveController(TestController):
 
     def test_index(self):
-        response = self.app.get(url_for(controller='archive'))
+        response = self.app.get(url(controller='archive', action='index'))
         # Test response...
--- web/app/snapshot/tests/functional/test_root.py
+++ web/app/snapshot/tests/functional/test_root.py
@@ -3,5 +3,5 @@ from snapshot.tests import *
 class TestRootController(TestController):
 
     def test_index(self):
-        response = self.app.get(url_for(controller='root'))
+        response = self.app.get(url(controller='root', action='index'))
         # Test response...
