bfe07d84a07b12442ae040f2cf5d320d746c9bc6
Peter Palfrader <peter@palfrader.org>
2010-02-24 11:36:16 +0100

    And create types and languages

--- db/db-init.sql
+++ db/db-init.sql
@@ -5,3 +5,11 @@ GRANT CONNECT, TEMPORARY ON DATABASE "snapshot" TO PUBLIC;
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 GRANT ALL ON SCHEMA public TO "snapshot";
 GRANT USAGE ON SCHEMA public TO PUBLIC;
+
+
+-- next, create the debversion types:
+\i /usr/share/postgresql/8.4/contrib/debversion.sql
+
+-- and the perl language
+CREATE LANGUAGE plperl;
+CREATE LANGUAGE plpgsql;
