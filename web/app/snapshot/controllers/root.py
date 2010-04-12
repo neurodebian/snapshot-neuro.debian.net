46eaa6b754a4e0c46595062e0dcb6d834d72d508
Peter Palfrader <peter@palfrader.org>
2010-04-12 15:19:32 +0200

    Import config from pylons

--- web/app/snapshot/controllers/root.py
+++ web/app/snapshot/controllers/root.py
@@ -1,6 +1,6 @@
 import logging
 
-from pylons import request, response, session, tmpl_context as c, g
+from pylons import request, response, session, tmpl_context as c, g, config
 from pylons.controllers.util import abort, redirect_to
 
 from snapshot.lib.base import BaseController, render
