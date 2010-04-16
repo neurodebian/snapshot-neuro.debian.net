15c4c8d127930631792ede653df3dabd3975ae4a
Peter Palfrader <peter@palfrader.org>
2010-04-16 15:26:58 +0000

    Update app/setup.py since we no longer need DBUtils

--- web/app/setup.py
+++ web/app/setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         "Pylons>=0.9.7",
         "Mako",
-        "DBUtils>=1.0",
+        "psycopg2>=2.0.7"
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
